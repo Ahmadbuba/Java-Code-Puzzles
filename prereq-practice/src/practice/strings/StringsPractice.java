a62fcf447ce86eebb64c8a93aa06ee8339a7b94e
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-11-30 09:33:52 +0100

    concept- equals to with strings in java

--- prereq-practice/src/practice/strings/StringsPractice.java
+++ prereq-practice/src/practice/strings/StringsPractice.java
@@ -4,13 +4,11 @@ public class StringsPractice {
 	public static void main(String[] args) {
 //		System.out.println("hello world");
 		
-		int mikesAge = 10;
-		int bobsAge = 12;
+		String a = "hello";
+		String b = "there";
 		
-		if(mikesAge != bobsAge) {
-			System.out.println("ages are different");
-		} else {
-			System.out.println("ages are same");
+		if(a.equals("hello")) {
+			
 		}
 	}
 }
