16f2291a3240a25733d881e7e21c98d847aabaaf
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-11-30 10:05:59 +0100

    concept and mini assignment- looked at expressions resulting to booleans and did a mini assignment on dogTrouble

--- prereq-practice/src/practice/strings/StringsPractice.java
+++ prereq-practice/src/practice/strings/StringsPractice.java
@@ -4,16 +4,34 @@ public class StringsPractice {
 	public static void main(String[] args) {
 //		System.out.println("hello world");
 		
-		String str = "Hello there yogi there";
+		boolean hungry = (3<5);
 		
-		int a = str.indexOf("there",7);
 		
-		int d = str.lastIndexOf("there");
+		if(getValue()) {
+			System.out.println("pigout");
+		} else if(hungry == false) {
+			System.out.println("I am already full");
+		}
+		else {
+			System.out.println("I'm sleeping I don't care about hunger");
+		}
 		
-		int b = str.indexOf("there");
-		
-		System.out.println(a);
-		System.out.println(d);
-		System.out.println(b);
+		System.out.println(dogTrouble(true,6)); // true
+		System.out.println(dogTrouble(true,7)); // false
+		System.out.println(dogTrouble(false,6)); // false
+	
+	}
+	
+	public static boolean getValue() {
+		return true;
+	}
+	
+	
+	public static boolean dogTrouble(boolean barking, int hour) {
+		if(barking && (hour < 7 || hour > 20)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
